d2e1166f0f154dba376c2a3bc92caa424c875bd0
Abdulrehman-TRI <webdeveloper.mb@gmail.com>
2025-12-17 18:25:40 +0530

    Update response message to include greeting

--- node-hello-world/index.js
+++ node-hello-world/index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World 🚀");
+    res.end("Hello World , Hey Abdul");
 });
 
 server.listen(PORT, () => {
